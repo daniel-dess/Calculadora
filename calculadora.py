2512df5d7d440a3d0d736b90eb8d85e173c6f82f
Daniel Elias <danielelias3001@gmail.com>
2022-08-09 19:56:39 -0500

    adicionar os operadores básicos #2

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,14 @@
 number_1 = int(input('Enter your first number: ')) 
 number_2 = int(input('Enter your second number: '))
+
+# Sum
 print (f'{number_1} + {number_2} = {number_1+number_2}')
+
+# Subtraction
+print (f'{number_1} - {number_2} = {number_1-number_2}')
+
+#Multiplication 
+print (f'{number_1} * {number_2} = {number_1*number_2}')
+
+# Division
+print (f'{number_1} / {number_2} = {number_1+number_2}')
